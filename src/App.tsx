92ff7fd13c3f5510c634a189a9c5c1d05ef28926
Ricardo Ferro Moreno <rferromoreno@gmail.com>
2026-01-19 05:27:52 -0300

    Use alias imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import './App.css'
-import HeroSection from './components/home/HeroSection';
-import ForYouSection from './components/home/ForYouSection';
-import BenefitsSection from './components/home/BenefitsSection';
-import CoursesSection from './components/home/CoursesSection';
-import HowItWorksSection from './components/home/HowItWorksSection';
-import TeachersSection from './components/home/TeachersSection';
-import TestimonialsSection from './components/home/TestimonialsSection';
-import FAQSection from './components/home/FAQSection';
-import ContactSection from './components/home/ContactSection';
-import Footer from './components/home/Footer';
+import '@/App.css'
+import HeroSection from '@/components/home/HeroSection';
+import ForYouSection from '@/components/home/ForYouSection';
+import BenefitsSection from '@/components/home/BenefitsSection';
+import CoursesSection from '@/components/home/CoursesSection';
+import HowItWorksSection from '@/components/home/HowItWorksSection';
+import TeachersSection from '@/components/home/TeachersSection';
+import TestimonialsSection from '@/components/home/TestimonialsSection';
+import FAQSection from '@/components/home/FAQSection';
+import ContactSection from '@/components/home/ContactSection';
+import Footer from '@/components/home/Footer';
 
 export default function Home() {
   return (
